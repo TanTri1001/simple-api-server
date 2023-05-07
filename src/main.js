a2a660b848b8f84709409d33fc3a6c4d19b70349
amili <tanyaluk.tri@gmail.com>
2023-05-07 21:14:14 +0200

    write post route

--- src/main.js
+++ src/main.js
@@ -7,28 +7,48 @@ app.use(express.json());
 
 const DATA_FILE = 'shopping-list.json';
 
+async function getShoppingList() {
+    const text = await promises.readFile(DATA_FILE, {encoding: 'utf8'})
+    return JSON.parse(text)
+}
+
+async function saveShoppingList(shoppingList) {
+    await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding:'utf-8'})
+}
 /**
  * Return all shopping list items as JSON.
  */
 app.get('/items', async (request, response) => {
-    const text = await promises.readFile(DATA_FILE, {encoding:'utf8'})
-    const shoppingList = JSON.parse(text);
+    const shoppingList = await getShoppingList()
     response.json(shoppingList);
+})
+/**
+ * save a new shopping list item
+ */
+app.post('/items', async (request, response) =>{
+    const shoppingList = await getShoppingList();
+
+    const newItem = request.body;
+    console.log(newItem)
+    shoppingList.push(newItem)
+
+    await saveShoppingList(shoppingList)
+
+    response.sendStatus(200)
 })
 
 /**
  * Override a shopping list item at a given position.
  */
 app.put('/items/:index', async (request, response) => {
-    const data = await promises.readFile(DATA_FILE, {encoding:'utf8'})
-    const shoppingList = JSON.parse(data);
+    const shoppingList = await getShoppingList()
 
     const newItem = request.body;
     const itemIndex = request.params['index'];
 
     shoppingList[itemIndex] = newItem;
 
-    await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding:'utf-8'})
+    await saveShoppingList(shoppingList)
 
     response.sendStatus(200);
 })
@@ -37,14 +57,13 @@ app.put('/items/:index', async (request, response) => {
  * Delete a shopping list item from a given position.
  */
 app.delete('/items/:index', async (request, response) => {
-    const data = await promises.readFile(DATA_FILE, {encoding:'utf8'})
-    const shoppingList = JSON.parse(data);
+    const shoppingList = await getShoppingList()
 
     const itemIndex = request.params['index'];
 
     shoppingList.splice(itemIndex, 1)
 
-    await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding:'utf-8'})
+    await saveShoppingList(shoppingList)
 
     response.sendStatus(200);
 })
